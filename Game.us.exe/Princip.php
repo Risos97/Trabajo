65194d83eaad0878d9b6362dd0f86f71a94ab5af
Risos97 <64970374+Risos97@users.noreply.github.com>
2020-05-14 15:19:44 +0200

    Add files via upload

--- Game.us.exe/Princip.php
+++ Game.us.exe/Princip.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+	include_once ("estilo.css");
+?>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -6,48 +9,6 @@
 body {font-family: Arial, Helvetica, sans-serif;}
 * {box-sizing: border-box;}
 
-.bg-img {
-  /* The image used */
-  background-image: url("images/wooden.jpg");
-
-  min-height: 380px;
-
-  /* Center and scale the image nicely */
-  background-position: absolute;
-  background-repeat: no-repeat;
-  background-size: cover;
-  
-  /* Needed to position the navbar */
-  position: relative;
-}
-
-/* Position the navbar container inside the image */
-.container {
-  position: absolute;
-  margin: 20px;
-  width: auto;
-}
-
-/* The navbar */
-.topnav {
-  overflow: hidden;
-  background-color: black;
-}
-
-/* Navbar links */
-.topnav a {
-  float: left;
-  color: yellow;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: red;
-}
 </style>
 </head>
 <body>
@@ -58,7 +19,11 @@ body {font-family: Arial, Helvetica, sans-serif;}
     <div class="topnav">
       <a href="formulario.php">Registro</a>
       <a href="producto.php">Producto</a>
-       <a href="alta_producto.php">A-Producto</a>
+      <a href="login_empleado.php">Login</a>
+      <a href="perfil.php">Perfil</a>
+      <a href="alta_producto.php">A-Producto</a>
+      
+      
       
     </div>
   </div>
