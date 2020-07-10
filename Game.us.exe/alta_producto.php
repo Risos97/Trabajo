b34947af7dbc8a3f58f2d9d47a30c301ee2d7ac6
Aeccio <65340566+Aeccio@users.noreply.github.com>
2020-07-10 17:56:51 +0200

    Add files via upload

--- Game.us.exe/alta_producto.php
+++ Game.us.exe/alta_producto.php
@@ -4,13 +4,13 @@
 	include_once ("estilo.css"); 
 	
 if (!isset($_SESSION["producto"])) {
-		$formulario['idn'] = "";
-		$formulario['precio'] = "";
-		$formulario['nombre'] = "";
-		$formulario['descripcion'] = "";
-		$formulario['stock'] = "";
-		$formulario['fechaLanzamiento'] = "";
-		$formulatio['tipo'] = "";
+		$producto['idn'] = "";
+		$producto['precio'] = "";
+		$producto['nombre'] = "";
+		$producto['descripcion'] = "";
+		$producto['stock'] = "";
+		$producto['fechaLanzamiento'] = "";
+		$producto['tipo'] = "";
 		
 	
 		$_SESSION["producto"] = $producto;
@@ -34,13 +34,24 @@ if (!isset($_SESSION["producto"])) {
 </head>
 
 <body>
-	<script>
-		$(document).ready(function() {
-			$("#altaProducto").on("submit", function() {
-				return validateForm();
-			});
-	</script>
 	
+		<script function validaciónFechaJuego(){
+	var date = document.getElementById("launch");
+	var aux = date.value;
+	
+	var ahora = new Date();
+	var fJuego= new Date(aux);
+	document.write(ahora);
+	if(ahora>fJuego){
+		document.write(fJuego);
+		var error= "Una fecha de lanzamiento de un juego tiene que ser posterior a ahora";
+	}
+	
+	launch.setCustomValidity(error);
+	
+	return error;
+}></script>
+
 	<?php
 		include_once("cabecera.php");
 	?>
@@ -55,50 +66,55 @@ if (!isset($_SESSION["producto"])) {
   		}
 	?>
 	
-	<form id="altaProducto" method="get" action="validacion_alta_producto.php"
+	<form id="altaProducto" method="get" action="validacion_alta_producto.php" onsubmit="return validaciónFechaJuego()"
 	>
 			<p><i>Los campos obligatorios están marcados con </i><em>*</em></p>
 			<fieldset><legend>Datos Producto</legend>				
 					<div><label for="idn">IDN:<em>*</em></label>
-					<input id="idn" name="idn" type="text" size="20" value="<?php echo $formulario['idn'];?>" required/>
+					<input id="idn" name="idn" type="text" size="20" value="<?php echo $producto['idn'];?>" required/>
 					</div>
 					
 					<div><label for="precio">Precio<em>*</em></label>
-					<input id="precio" name="precio" type="number" min = "0" value="<?php echo $formulario['precio'];?>" required>
+					<input id="precio" name="precio" type="number" min = "0" value="<?php echo $producto['precio'];?>" required>
 					</div>
 					
 					<div><label for="nombre">Nombre:<em>*</em></label>
-					<input id="nombre" name="nombre" type="text" size="20" value="<?php echo $formulario['nombre'];?>" required/>
+					<input id="nombre" name="nombre" type="text" size="20" value="<?php echo $producto['nombre'];?>" required/>
 					</div>
 				
 					<div><label for="descripcion">Descripción:<em>*</em></label>
-					<input id="descripcion" name="descripcion" type="text" size="50" value="<?php echo $formulario['descripcion'];?>" required/>
+					<input id="descripcion" name="descripcion" type="text" size="50" value="<?php echo $producto['descripcion'];?>" required/>
 					</div>
 				
 					<div><label for="stock">Stock<em>*</em></label>
-					<input id="stock" name="stock" type="number" min = "0" value="<?php echo $formulario['stock'];?>" required>
+					<input id="stock" name="stock" type="number" min = "0" value="<?php echo $producto['stock'];?>" required>
 					</div>
 				
 					<div><label>Fecha de lanzamiento:</label>
-					<input type="date" id="launch" name="fechaLanzamiento" title="Introduzca la fecha de lanzamiento" value="<?php echo $formulario['fechaLanzamiento'];?>" required>
+					<input type="date" id="launch" name="fechaLanzamiento" title="Introduzca la fecha de lanzamiento" value="<?php echo $producto['fechaLanzamiento'];?>" required oninput="birthDateValidation();">
 					</div>
 					
 					<div><label for="tipo">Escoge un tipo:</label>
 
-					<select id="tipo" required>
-  					<option value="VIDEOJUEGO">Videojuego</option>
-  					<option value="MERCHANDISING">Merchandising</option>
-  					<option value="PLATAFORMAS">Plataforma</option>
-					</select>
+					<input type="radio" name="tipo" value="videojuego">VIDEOJUEGO
+					<input type="radio" name="tipo" value="consola">CONSOLA
+					<input type="radio" name="tipo" value="merchandising">MERCHANDISING
+		
 					</div>
 				</fieldset>
 			<div><input type="submit" value="Enviar" /></div>
 		</form>
 		
 		<?php
+	
 		include_once("pie.php");
 		cerrarConexionBD($conexion);
 		?>
+		
+      
+    
+      
+     
 		
 	</body>
 </html>
--- Game.us.exe/validacion_alta_producto.php
+++ Game.us.exe/validacion_alta_producto.php
@@ -1,15 +1,16 @@
 <?php
 	session_start();
+	require_once("gestionBD.php");
 	
 	if (isset($_SESSION["producto"])) {
 		// Recogemos los datos del formulario
-		$formulario['idn'] = $_REQUEST["idn"];
-		$formulario['precio'] = $_REQUEST["precio"];
-		$formulario['nombre'] = $_REQUEST["nombre"];
-		$formulario['descripcion'] = $_REQUEST["descripcion"];
-		$formulario['stock'] = $_REQUEST["stock"];
-		$formulario['fechaLanzamiento'] = $_REQUEST["fechaLanzamiento"];
-		$formulario['tipo'] = $_REQUEST["tipo"];
+		$nuevoProducto['idn'] = $_REQUEST["idn"];
+		$nuevoProducto['precio'] = $_REQUEST["precio"];
+		$nuevoProducto['nombre'] = $_REQUEST["nombre"];
+		$nuevoProducto['descripcion'] = $_REQUEST["descripcion"];
+		$nuevoProducto['stock'] = $_REQUEST["stock"];
+		$nuevoProducto['fechaLanzamiento'] = $_REQUEST["fechaLanzamiento"];
+		$nuevoProducto['tipo'] = $_REQUEST["tipo"];
 		
 		
 		// Guardar la variable local con los datos del formulario en la sesión.
@@ -35,9 +36,13 @@
 function validarDatosProducto($conexion, $nuevoProducto){
 	$errores=array();
 	
-	// Validación del IDN			
-	if($nuevoUsuario["IDN"]=="") 
+	// Validación del idn			
+	if($nuevoUsuario["idn"]=="") 
 		$errores[] = "<p>El IDN no puede estar vacío</p>";
+	
+	// Validación del precio			
+	if($nuevoUsuario["precio"]=="") 
+		$errores[] = "<p>El precio no puede estar vacío</p>";
 		
 	// Validación del nombre		
 	if($nuevoUsuario["nombre"]=="") 
@@ -47,11 +52,17 @@ function validarDatosProducto($conexion, $nuevoProducto){
 	if($nuevoUsuario["descripcion"]=="") 
 		$errores[] = "<p>La descripcion no puede estar vacía</p>";
 	
-	// Validación del Nickname			
-	if($nuevoUsuario["nickname"]=="") 
-		$errores[] = "<p>El nickname no puede estar vacío</p>";
+	// Validación del stock			
+	if($nuevoUsuario["stock"]=="") 
+		$errores[] = "<p>El stock no puede estar vacío</p>";
 	
+	// Validación de la fecha de lanzamiento			
+	if($nuevoUsuario["fechaLanzamiento"]=="") 
+		$errores[] = "<p>La fecha lanzamiento no puede estar vacía</p>";
 	
+	// Validación del tipo		
+	if($nuevoUsuario["tipo"]=="") 
+		$errores[] = "<p>El tipo no puede estar vacío</p>";
 	
 }
 
