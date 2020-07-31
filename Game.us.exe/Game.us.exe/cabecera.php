ab0aa8ac158eb38393dad8b66c2aad2caced8157
Misterepv <44266158+Misterepv@users.noreply.github.com>
2020-07-31 16:49:36 +0200

    Delete cabecera.php

--- Game.us.exe/Game.us.exe/cabecera.php
+++
@@ -1,9 +0,0 @@
-<header>
-	<input type="image" id ="cabecera" src="images/logo.jpg" alt="Game.us.exe" href="Princip.php">/>
-    <a href="Princip.php" id="cabecera" alt="Game.us.exe"> </a>
-     
-      
-      
-      
-  
-</header>
