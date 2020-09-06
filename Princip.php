a6c8e067cd96d60f5248b14ed241690973fad3af
Ervaldy <63309657+Ervaldy@users.noreply.github.com>
2020-09-06 16:19:45 +0200

    Add files via upload

---
+++ Princip.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<?php
+	include_once ("estilo.css");
+	
+?>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+</style>
+</head>
+<body>
+
+<?php
+	
+	include_once ("cabecera.php");
+?>
+<h2>Games.us.exe</h2>
+<h3>Tu catálogo online de videojuegos</h3>
+<div class="bg-img">
+  <div class="container">
+    <div class="topnav">
+      <a href="formulario.php">Registro</a>
+      <a href="consulta_producto_usuario.php">Producto</a>
+      <a href="login_empleado.php" >Login Usuario</a>
+      <a href="login_empleado2.php" >Login Empleado</a>
+      <a href="perfil.php">Perfil</a>
+      <a href="Sobre_nosotros.php">Sobre nosotros</a>
+      
+      
+      
+    </div>
+  </div>
+</div>
+
+</body>
