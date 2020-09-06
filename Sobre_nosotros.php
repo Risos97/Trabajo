80cc3be4322becce2201f2369aca898daeaef88e
Ervaldy <63309657+Ervaldy@users.noreply.github.com>
2020-09-06 16:22:09 +0200

    Add files via upload

---
+++ Sobre_nosotros.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<?php
+	include_once ("estilo.css");
+	
+?>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Sobre nosotros</title>
+</head>
+
+<body text="lightblue">
+	<div style="padding-left:100px;"><font color="lightblue" size=11px><strong>Sobre nosotros...</strong></font><br/>
+	<br/>
+	<div style="padding-left:20px;">Somos cuatro alumnos de la asignatura IISSI2 (Víctor Alonso Sánchez, Cristian Aragó</div>
+	Silva, Enrique Piedra Venegas y José Antonio Valdivieso Casado), como proyecto hemos<br/>
+	decidido hacer una tienda online de productos de entretenimiento, principalmente<br/>
+	consolas, videojuegos y merchandising (posters, figuras, tazas, etc.…).<br/>
+	<br/>
+	<br/>
+	<font color="lightblue" size=11px><strong>Funcionamiento de la página...</strong></font><br/>
+	<br/>
+	<div style="padding-left:20px;">La página está creada en PHP, HTML y CSS (éste último para darle estilo a la página)</div>
+	usando los programas AptanaStudio para crear el código y XAMPP para levantar un<br/>
+	“servidor” en Apache, está asociada a una base de datos en la que introducimos los diversos<br/>
+	elementos necesarios para el funcionamiento de la página (productos, usuarios,<br/>
+	empleados...), la base de datos fue creada y es mantenida en SQLDeveloper.<br/>
+	<br/>
+	<div style="padding-left:20px;">Contiene una página principal con enlaces a las demás funciones de la página, como</div>
+	el acceso a los productos, log in para los usuarios, log in para los empleados, registro (para<br/>
+	añadir nuevos usuarios a la base de datos) y acceso al perfil del usuario. Contiene funciones<br/>
+	para acceder a la lista de productos en la base de datos y, si eres un empleado, puedes<br/>
+	añadir nuevos productos y modificar o eliminar los ya existentes. <br/>
+	<br/>
+	<div style="padding-left:20px;">Mediante JavaScript implementamos las funciones pertinentes para cumplir</div>
+	nuestros requisitos a la hora de poner una contraseña, la fecha de salida de un producto y el<br/>
+	precio de éste. También usamos JavaScript para implementar los botones usados en la<br/></div>
+</body>
